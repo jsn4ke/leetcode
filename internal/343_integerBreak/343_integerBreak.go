66e957e81307b198e49328776e27c34d46ed2208
jsn4ke <mail.caoke@gmail.com>
2024-06-08 22:20:52 +0800

    resolve

---
+++ internal/343_integerBreak/343_integerBreak.go
@@ -0,0 +1,12 @@
+package leetcode_343_integerBreak
+
+func integerBreak(n int) int {
+	var dp = make([]int, n+1)
+	dp[1] = 1
+	for i := 2; i <= n; i++ {
+		for j := 1; j < i; j++ {
+			dp[i] = max(dp[i], max(j*(i-j), j*dp[i-j]))
+		}
+	}
+	return dp[n]
+}
