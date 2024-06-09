71a090ce3ff45479f70e3e96c250f7d772b9a3d0
jsn4ke <mail.caoke@gmail.com>
2024-06-09 16:07:40 +0800

    287_findDuplicate

---
+++ internal/287_findDuplicate/287_findDuplicate.go
@@ -0,0 +1,21 @@
+package leetcode_287_findDuplicate
+
+func findDuplicate(nums []int) int {
+	slow, fast := 0, 0
+	for {
+		slow = nums[slow]
+		fast = nums[nums[fast]]
+		if slow == fast {
+			break
+		}
+	}
+	fast = 0
+	for slow != fast {
+		slow = nums[slow]
+		fast = nums[fast]
+	}
+	return slow
+}
+
+func findDuplicate2(nums []int) int {
+}
