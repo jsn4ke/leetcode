3b0d8136dabff0b5859a8509e0cd596652cb3c7e
jsn4ke <mail.caoke@gmail.com>
2024-06-09 17:55:47 +0800

    20_isValid

---
+++ internal/20_isValid/20_isValid.go
@@ -0,0 +1,21 @@
+package leetcode_20_isValid
+
+func isValid(s string) bool {
+	var stack []byte
+	for i := range s {
+		switch s[i] {
+		case '(':
+			stack = append(stack, ')')
+		case '[':
+			stack = append(stack, ']')
+		case '{':
+			stack = append(stack, '}')
+		case ')', ']', '}':
+			if len(stack) == 0 || stack[len(stack)-1] != s[i] {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+	return len(stack) == 0
+}
