cff95173f93a72052f4d84c75080c433a8756f0e
jsn4ke <mail.caoke@gmail.com>
2024-06-10 16:52:35 +0800

    update  (136_singleNumber)

--- ci.sh
+++ ci.sh
@@ -1,2 +1,5 @@
-branch=$1
+parse_git_branch() {
+            git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+    }
+branch=$(parse_git_branch)
 git add . && git ci -m "update $branch" && git push --set-upstream origin $branch
