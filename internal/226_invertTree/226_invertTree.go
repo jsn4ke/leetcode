ee87104ef08e177417ec7fe94d6c41b8581a3ec7
jsn4ke <mail.caoke@gmail.com>
2024-06-10 17:14:06 +0800

    update 226_invertTree

---
+++ internal/226_invertTree/226_invertTree.go
@@ -0,0 +1,18 @@
+package leetcode_226_invertTree
+
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if nil == root {
+		return nil
+	}
+	root.Left, root.Right = root.Right, root.Left
+	invertTree(root.Left)
+	invertTree(root.Right)
+	return root
+}
