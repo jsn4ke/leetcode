0b29c984924df4ccbb6bf4f5a29e4d554a93aa7d
cao ke <154774294+jsn4ke@users.noreply.github.com>
2024-06-11 21:16:48 +0800

    update 104_maxDepth (#54)

---
+++ internal/104_maxDepth/104_maxDepth.go
@@ -0,0 +1,17 @@
+package leetcode_104_maxDepth
+
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	if nil == root {
+		return 0
+	}
+	left := maxDepth(root.Left)
+	right := maxDepth(root.Right)
+	return 1 + max(left, right)
+}
