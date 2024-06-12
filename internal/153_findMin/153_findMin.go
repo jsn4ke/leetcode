13f46a4ca2208af9bbbe18373f373d10a90ce124
cao ke <154774294+jsn4ke@users.noreply.github.com>
2024-06-12 22:54:59 +0800

    update 153_findMin (#70)

---
+++ internal/153_findMin/153_findMin.go
@@ -0,0 +1,14 @@
+package leetcode_153_findMin
+
+func findMin(nums []int) int {
+	low, high := 0, len(nums)-1
+	for low < high {
+		pivot := low + (high-low)/2
+		if nums[pivot] < nums[high] {
+			high = pivot
+		} else {
+			low = pivot + 1
+		}
+	}
+	return nums[low]
+}
